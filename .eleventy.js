e596bc47ee072e15b71d63a8fcfeee2ca41bce27
zulvkr <ivan.zulf.ikar@gmail.com>
2021-01-17 11:36:54 +0700

    .eleventy.js is now tracked in git

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = function (config) {
+
+    config.addWatchTarget("src/_tmp/style.css");
+
+    if (!process.env.ELEVENTY_PRODUCTION) 
+    {
+      config.addPassthroughCopy({ "src/_tmp/style.css": "./style.css" });
+    }
+
+    return {
+          dir: {
+          input: "src"
+        }
+      };
+}
